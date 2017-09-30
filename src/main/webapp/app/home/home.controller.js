b1ebfbd4e094b7fafcf014cea88e7681a1a91679
pcastel <pierre.castel.84@gmail.com>
2017-09-30 23:12:22 +0200

    poc angular md calendar

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,9 +5,9 @@
         .module('scmApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', '$filter'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($scope, Principal, LoginService, $state, $filter) {
         var vm = this;
 
         vm.account = null;
@@ -29,5 +29,25 @@
         function register () {
             $state.go('register');
         }
+
+        vm.selectedDate = new Date();
+        vm.firstDayOfWeek = 0;
+        vm.setDirection = function(direction) {
+        vm.direction = direction;
+        };
+        vm.dayClick = function(date) {
+          $scope.msg = "You clicked " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
+        };
+        vm.prevMonth = function(data) {
+          $scope.msg = "You clicked (prev) month " + data.month + ", " + data.year;
+        };
+        vm.nextMonth = function(data) {
+          $scope.msg = "You clicked (next) month " + data.month + ", " + data.year;
+        };
+        vm.setDayContent = function(date) {
+          // You would inject any HTML you wanted for
+          // that particular date here.
+            return "<p></p>";
+        };
     }
 })();
